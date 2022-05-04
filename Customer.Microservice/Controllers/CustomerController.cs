e8e018373fc41fa19f2ef3a7f65e1a860bc20618
Daniele Calbucci <d.calbucci@systemretail.it>
2022-05-04 02:06:25 +0200

    first commit

---
+++ Customer.Microservice/Controllers/CustomerController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Customer.Microservice.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        public CustomerController()
+        {
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            return Ok("Customer Microservice executed !");
+        }
+    }
+}
